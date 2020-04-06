883579f2891901d6d424706273c000e25ec0812c
Garrett Griffey <garrettwgriffey@gmail.com>
2020-04-05 21:21:50 -0500

    finish markup

--- index.js
+++ index.js
@@ -97,8 +97,10 @@ function inquireQuestions() {
       apiCall(username, answers);
     });
 }
+
 inquireQuestions();
 
+// Github API
 function apiCall(username, answers) {
   const url = `https://api.github.com/users/` + username;
 
@@ -114,56 +116,56 @@ function apiCall(username, answers) {
 
 // module.exports = apiCall;
 
+// Markdown file
 function generateMD(answers) {
   var userInfo = ` 
-### **Contributers**
-${answers.contributers}
+  # **Project** ${answers.project}
+  ## Live Link
+  ##  **Table of Contents**
+    * Description
+    * Installation
+    * Technology Stack
+    * Usage
+  ###  **Contributors**
+  ${answers.contributors}
+  ##  **Description**
+  ${answers.description}
+  ## **Installation**
+  ${answers.installation}
+  ## **Technology Stack**
+  ${answers.technology}
+  ##  **Usage**
+  ${answers.usage}
+  ## **Contact**
+*
+<img align="left" width="100" height="100" src="${response.data.avatar_url}">
 
-##  **Description**
-${answers.description}
+<br/>
+## --------------------------------------------------------------------
+* ####  Name: ${response.data.firstLast}
+* ####   GitHub "https://github.com/users/${answers.username}"
+* ####  ${answers.portfolio}
+* #### Email: [${response.data.email}](${response.data.email})
+* #### LinkedIn: "https://www.linkedin.com/in/${answers.linkedIn}
+## -------------------------------------------------------------------
+<br/>
 
-## **Installation**
-${answers.installation}
+##### **License**
+${answers.license}
+## Tests;
 
-## **Technology Stack**
-${answers.technology}
+// Badge
 
-##  **Usage**
-${answers.usage}
+`;
+// End markup
 
-####   GitHub "https://github.com/users/${answers.username}"
+fs.writeFile("README.md", userInfo, function(err) {
+    if (err) {
+    return console.log(err);
+    }
+    console.log("Success!");
+  });
 
-    `;
 }
 
-// return
-//   <!DOCTYPE html>
-//   <html lang="en">
-//   <head>
-//     <meta charset="UTF-8">
-//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-//     <title>Document</title>
-//   </head>
-//   <body>
-//     <div class="jumbotron jumbotron-fluid">
-//     <div class="container">
-//       <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-//       <p class="lead">Github username: ${answers.username}.</p>
-//       <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-//       <ul class="list-group">
-//         <li class="list-group-item">Project name: ${answers.project}</li>
-//         <li class="list-group-item">Description: ${answers.description}</li>
-//         <li class="list-group-item">Installation: ${answers.installation}</li>
-//         <li class="list-group-item">Technology used: ${answers.technology}</li>
-//         <li class="list-group-item">Usage: ${answers.usage}</li>
-//         <li class="list-group-item">License: ${answers.license}</li>
-//         <li class="list-group-item">Contributors: ${answers.contributers}</li>
-//         <li class="list-group-item">LinkedIn username: ${answers.linkedin}</li>
-//         <li class="list-group-item">Portfolio: ${answers.portfolio}</li>
-//         <li class="list-group-item">Tests: ${answers.tests}</li>
-//       </ul>
-//     </div>
-//   </div>
-//   </body>
-//   </html>`
+
